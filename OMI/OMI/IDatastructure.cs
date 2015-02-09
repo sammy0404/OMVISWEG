1365e2607c5cb74e62ec433a127670fa3cd53535
knudde1 <G.H.aalvanger@students.uu.nl>
2015-02-09 11:12:08 +0100

    Interfeest

---
+++ OMI/OMI/IDatastructure.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace OMI
+{
+    interface IDatastructure
+    {
+        IDatastructure Build(List<KeyValuePair<int, object>> values);
+
+        bool TryAdd(KeyValuePair<int, object> kvp);
+        bool TryDelete(int i);
+
+        object Search(int i);
+
+        object GetMin();
+        object GetMax();
+
+        object ExtractMin();
+        object ExtractMax();
+    }
+}
